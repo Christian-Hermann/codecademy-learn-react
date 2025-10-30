cf4ba38f6e5b05e75dd6de04b2e8e8452d7b3a45
Your NameChristian <christianhermanndev@gmail.com>
2025-10-30 13:55:32 -0500

    Passing a variable to render

--- notes/JSX.js
+++ notes/JSX.js
@@ -8,3 +8,27 @@ const myDiv = (
     <h1>Hello world</h1>
   </div>
 );
+
+// JSX Outer Elements
+const blog = (
+  <div>
+    <img src="pics/192940u73.jpg" />
+    <h1>Welcome to Dan's Blog!</h1>
+    <article>
+      Wow I had the tastiest sandwich today. I <strong>literally</strong> almost
+      freaked out.
+    </article>
+  </div>
+);
+
+// passing a Variable to render
+const toDoList = (
+  <ol>
+    <li>Learn React</li>
+    <li>Become a Developer</li>
+  </ol>
+);
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(toDoList);
