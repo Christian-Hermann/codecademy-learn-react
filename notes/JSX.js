c1022a71931c35de984ae702fb2cfcd512ca758f
Your NameChristian <christianhermanndev@gmail.com>
2025-11-03 15:36:06 -0600

    Ternary in JSX

--- notes/JSX.js
+++ notes/JSX.js
@@ -78,3 +78,39 @@ const kitty = (
     onClick={makeDoggy}
   />
 );
+
+// JSX CONDITIONALS: IF STATEMENTS THAT DO WORK
+function coinToss() {
+  // This function will randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? "heads" : "tails";
+}
+
+const pics = {
+  kitty: "https://content.codecademy.com/courses/React/react_photo-kitty.jpg",
+  doggy: "https://content.codecademy.com/courses/React/react_photo-puppy.jpeg",
+};
+let img;
+
+// if/else statement begins here:
+if (coinToss() === "heads") {
+  img = <img src={pics.kitty} />;
+} else {
+  img = <img src={pics.doggy} />;
+}
+
+root.render(img);
+
+// Ternary
+function coinToss() {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? "heads" : "tails";
+}
+
+const pics = {
+  kitty: "https://content.codecademy.com/courses/React/react_photo-kitty.jpg",
+  doggy: "https://content.codecademy.com/courses/React/react_photo-puppy.jpeg",
+};
+
+const img = <img src={pics[coinToss() === "heads" ? "kitty" : "doggy"]} />;
+
+root.render(img);
