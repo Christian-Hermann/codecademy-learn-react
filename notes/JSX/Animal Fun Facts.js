ee5f2d01a77a96c3c825317f17fa86cdc58dd8e3
Your NameChristian <christianhermanndev@gmail.com>
2025-11-04 14:03:16 -0600

    Through #10

--- notes/JSX/Animal Fun Facts.js
+++ notes/JSX/Animal Fun Facts.js
@@ -6,8 +6,28 @@ const container = document.getElementById("app");
 const root = createRoot(container);
 
 const title = "";
+const background = (
+  <img className="background" src="/images/ocean.jpg" alt="ocean" />
+);
+const images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role="button"
+    />
+  );
+}
 const animalFacts = (
-  <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
+  <div>
+    {background}
+    <div className="animals">{images}</div>
+    <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
+  </div>
 );
 
 root.render(animalFacts);
