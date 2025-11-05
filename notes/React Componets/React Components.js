dc7a05779e75ffc287789f0ffcfb9661a1c0202c
Your NameChristian <christianhermanndev@gmail.com>
2025-11-05 13:19:21 -0600

    Using a variable attribute in component

--- notes/React Componets/React Components.js
+++ notes/React Componets/React Components.js
@@ -1,1 +1,20 @@
-// creating a function component
+// Using a Variable Attribute in a Component
+
+import React from "react";
+
+const owl = {
+  title: "Excellent Owl",
+  src: "https://content.codecademy.com/courses/React/react_photo-owl.jpg",
+};
+
+//Define function component here:
+function Owl() {
+  return (
+    <div>
+      <h1>{owl.title}</h1>
+      <img src={owl.src} alt={owl.title} />
+    </div>
+  );
+}
+
+export default Owl;
