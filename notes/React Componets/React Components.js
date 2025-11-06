bf20be11bce2f80d9e20cd75f9f430403155c7d1
Your NameChristian <christianhermanndev@gmail.com>
2025-11-06 11:25:27 -0600

    Conditionals and JSX

--- notes/React Componets/React Components.js
+++ notes/React Componets/React Components.js
@@ -50,3 +50,20 @@ function Friend() {
 }
 
 export default Friend;
+
+// Conditionals and JSX
+
+import React from "react";
+
+const fiftyFifty = Math.random() < 0.5;
+
+// New function component starts here:
+function TonightsPlan() {
+  if (fiftyFifty) {
+    return <h1>Tonight I'm going out WOOO</h1>;
+  } else {
+    return <h1>Tonight I'm going to bed WOOO</h1>;
+  }
+}
+
+export default TonightsPlan;
